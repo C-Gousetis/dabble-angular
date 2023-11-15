5cc344f25721ff5da11ba7b69d654d29b8deeae3
Catherine Gousetis <c.gousetis@yahoo.com>
2023-11-15 18:16:13 +0200

    homepage-message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeMessageComponent } from './home-message/home-message.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, NavbarComponent],
+  imports: [CommonModule, NavbarComponent, HomeMessageComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
---
+++ src/app/home-message/home-message.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home-message',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home-message.component.html',
+  styleUrls: ['./home-message.component.css']
+})
+export class HomeMessageComponent {
+
+}
