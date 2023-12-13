136c11eb3e42aa6e74a30d40062e869877363a98
Catherine Gousetis <c.gousetis@yahoo.com>
2023-12-13 21:50:33 +0200

    data import from api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,27 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeMessageComponent } from './home-message/home-message.component';
 import { ListResultComponent } from './list-result/list-result.component';
+import {HttpClientModule} from '@angular/common/http';
+import {NamesService} from './names.service';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, HomeMessageComponent, ListResultComponent],
+  imports: [CommonModule, NavbarComponent, HomeMessageComponent, ListResultComponent, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  
 })
 export class AppComponent {
   title = 'angular-dabble';
+
+  data: any;
+  constructor(private name: NamesService) {
+
+    this.name.getData().subscribe(data => {
+      console.warn(data);
+      this.data = data;
+    })
+  }
 }
